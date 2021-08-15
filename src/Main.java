dd46fb8a879302359314b52d915eca2d70dcd236
Victroria Anisimova <vitkakim@mail.ru>
2021-08-15 12:33:43 +0300

    inition commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 65; // стоимость билета
+        int price = 20; // сиоимость одной бонусной мили
+        int miles = cost / price; // количество миль
+        System.out.println("Мили = " + miles);
+
+    }
+}
